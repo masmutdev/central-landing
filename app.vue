e00ae95873ede3587e2d7fbbb9168f4db76623ba
Masmut Dev <masmutofficial@gmail.com>
2025-05-25 08:37:03 +0700

    update

--- app.vue
+++ app.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-useHead({
-  link: [
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@400;500;600;700&display=swap",
-    },
-  ],
+import { onMounted } from "vue";
+
+onMounted(async () => {
+  if (import.meta.client) {
+    const WebFont = await import("webfontloader");
+    WebFont.load({
+      google: {
+        families: ["Bebas Neue", "Poppins:400,500,600,700&display=swap"],
+      },
+    });
+  }
 });
 </script>
 
